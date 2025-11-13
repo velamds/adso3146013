dc0e39a5390a31f03c0b34d31f311b3322a6818f
Jhoan Ramirez <ramirezospinajhoanesteban@gmail.com>
2025-11-13 10:41:30 -0500

    estilo pra h1

--- view.php
+++ view.php
@@ -1,4 +1,5 @@
 <form action="" method="post">
+    <h1> hola</h1>
     <input type="text" name='nombre' placeholder="Nombre">
     <input type="email" name='correo' placeholder="correo">
     <input type="text" name="rol" placeholder="rol">
@@ -7,3 +8,9 @@
     <button type="submit">Guardar</button>
 
 </form>
+
+<style>
+    h1{
+        background-color: orange;
+    }
+</style>
